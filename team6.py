1bdf73b19a1140b81e28e7079260676e17a89af8
RokShox <ksease3637@greenvilleschools.us>
2018-03-19 13:56:25 -0400

    I don't even

--- team6.py
+++ team6.py
@@ -6,18 +6,16 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'RokShox' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
+    if their_history == 'c':
+        if my_score >= -10:
+            print ('b')
+        else:
+            print ('c')
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +24,6 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
